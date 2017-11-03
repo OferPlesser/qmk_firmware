2ed97fca5877e0971404bc21cfa42860512babc4
Ofer Plesser <plesserofer@gmail.com>
2017-11-03 10:57:20 +0200

    Update halite keyboard keymap

--- keyboards/halite/halite.h
+++ keyboards/halite/halite.h
@@ -3,26 +3,25 @@
 
 #include "quantum.h"
 
-inline void halite_fn1_led_off(void) 	{ DDRB |= (1<<5); PORTB &= ~(1<<5); }
-inline void halite_fn1_led_on(void) 	{ DDRB |= (1<<5); PORTB |= (1<<5); }
-inline void halite_fn0_led_off(void) 	{ DDRF |= (1<<0); PORTF &= ~(1<<0); }
-inline void halite_fn0_led_on(void) 	{ DDRF |= (1<<0); PORTF |= (1<<0); }
+#define LED_INIT(led_pin) \
+do { \
+    _SFR_IO8((led_pin >> 4) + 1) |= _BV(led_pin & 0xF); \
+    _SFR_IO8((led_pin >> 4) + 2) &= ~_BV(led_pin & 0xF); \
+} while(0)
 
-// This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguements
-// The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, \
-    k12, k13, k14, k15, k16, k17, k18, k19, k20, k22, k23, k24, \
-    k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, k36, \
-    		k37, k38, 					k49, k40 \
-) \
-{ \
-    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11 }, \
-    { k12, k13, k14, k15, k16, k17, k18, k19, k20, k22, k23, k24 }, \
-    { k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, k36, }, \
-    { KC_NO, KC_NO, k37, k38, KC_NO, KC_NO, KC_NO, KC_NO, k39, k40, KC_NO, KC_NO } \
-}
+#define LED_ON(led_pin)     _SFR_IO8((led_pin >> 4) + 2) |= _BV(led_pin & 0xF)
+
+#define LED_OFF(led_pin)    _SFR_IO8((led_pin >> 4) + 2) &= ~_BV(led_pin & 0xF)
+
+#define LAYER_ON(layer)     ((1<<layer) & layer_state)
+
+#define BIND_LED_TO_LAYER(led_pin, layer) \
+do { \
+    if (LAYER_ON(layer)) { \
+         LED_ON(led_pin); \
+    } else { \
+        LED_OFF(led_pin); \
+    } \
+} while(0)
 
 #endif
--- keyboards/halite/keymaps/default/keymap.c
+++ keyboards/halite/keymaps/default/keymap.c
@@ -1,132 +1,110 @@
 #include "halite.h"
-#include "action_layer.h"
 
 #define XXXXXXX     KC_NO
 #define _______     KC_TRNS
+
+/*** LAYERS ***/
 
 #define _QWERTY     0
 #define _SYMBOLS    1
 #define _NUMBERS    2
 #define _META       3
 
-#define KC_3_1      MT(MOD_LSFT, KC_BSPC)
-#define KC_2_5      MT(MOD_LALT, KC_ENT)
-#define KC_2_10     LT(1, KC_SLSH)
-#define KC_0_5      M(0)
+/*** ACTIONS ***/
 
-#define META_LAYER_TIMEOUT 3000
+#define SFT_TAB     MT(MOD_LSFT, KC_TAB)
+#define SFTNTAB     LSFT(KC_TAB)
+#define ENT_ALT     MT(MOD_LALT, KC_ENT)
+#define GUI_DOT     MT(MOD_LGUI, KC_PDOT)
+#define MS_ON       M(0)
+#define MS_OFF      M(1)
 
-static uint8_t  number_layer_already_on = 0;
-static uint8_t  meta_layer_already_on = 0;
-static uint16_t key_timer;
+/*** LEDS ***/
+
+#define FN_0_LED    F0
+#define FN_1_LED    B5
+
+/*** META ***/
+
+#define EXIT_MT     TO(_QWERTY)
+#define META_LAYER_COMBO_INDEX  0
+
+/*** TAP DANCE ***/
+
+#define BRCS        TD(TD_SQUARE_BRACKETS)
+#define CBRCS       TD(TD_CURLY_BRACKETS)
+#define PARENS      TD(TD_PARENTHESES)
+#define CHEVRS      TD(TD_CHEVRONS)
+#define QUOTS       TD(TD_QUOTES)
+
+enum {
+    TD_SQUARE_BRACKETS,
+    TD_CURLY_BRACKETS,
+    TD_PARENTHESES,
+    TD_CHEVRONS,
+    TD_QUOTES,
+};
+
+
+/*** KEYMAPS ***/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = { /* Base */
-    { KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_0_5, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P },
-    { KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
-    { KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_2_5, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_2_10 },
-    { XXXXXXX, KC_LCTL, KC_3_1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_RGUI, XXXXXXX }},
+    { KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_FN1,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
+    { KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    SFT_TAB, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
+    { KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ENT_ALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_FN0  },
+    { XXXXXXX, KC_LCTL, KC_BSPC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_RGUI, XXXXXXX }},
 
 [_SYMBOLS] = {
-    { KC_PGUP, KC_HOME,   KC_UP,  KC_END,  KC_INS, KC_ESC,  KC_EQL,  KC_LPRN, KC_LCBR, KC_LBRC, KC_QUOT },
-    { KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_PIPE, KC_PLUS, KC_RPRN, KC_RCBR, KC_RBRC, KC_DQUO },
-    { KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_UNDS, _______ },
+    { KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,  KC_ESC,  QUOTS,   PARENS,  CBRCS,   BRCS,    CHEVRS  },
+    { KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_PIPE },
+    { KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, _______ },
     { XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX }},
 
 [_NUMBERS] = {
-    { KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______, KC_PGUP, KC_7,    KC_8,    KC_9,    KC_ASTR },
-    { KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_SLCK, _______, KC_PGDN, KC_4,    KC_5,    KC_6,    KC_PLUS },
-    { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PAUS, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_BSLS },
-    { XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_DOT,  XXXXXXX }},
+    { KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______, KC_PAST, KC_P7,   KC_P8,   KC_P9,    KC_PMNS },
+    { KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_SLCK, SFTNTAB, KC_PSLS, KC_P4,   KC_P5,   KC_P6,    KC_PPLS },
+    { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PAUS, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,    KC_BSLS },
+    { XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, GUI_DOT,  XXXXXXX }},
 
 [_META] = {
-    { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-    { _______, _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______ },
-    { _______, _______, _______, _______, _______, BL_STEP, _______,    _______, _______, _______, _______ },
-    { XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX }}
+    { BL_TOGG, MS_ON,   XXXXXXX, XXXXXXX, XXXXXXX, EXIT_MT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX },
+    { BL_INC,  MS_OFF,  XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX },
+    { BL_DEC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX },
+    { XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX }}
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-
+    [0] = ACTION_LAYER_TAP_KEY(_SYMBOLS, KC_SLSH),
+    [1] = ACTION_LAYER_TAP_TOGGLE(_NUMBERS),
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-    switch(id) {
-        case 0:
-            // This macro makes it so the fn1 key is 
-            // both momentary and toggleable
-            if (record->event.pressed) {
-                key_timer = timer_read();
-                if (!meta_layer_already_on) {
-                    layer_on(_NUMBERS);
-                }
-            } else {
-                if (number_layer_already_on) {
-                    layer_off(_NUMBERS);
-                    number_layer_already_on = 0;
-                } else {
-                    if (meta_layer_already_on) {
-                        layer_off(_META);
-                        breathing_disable();
-                        halite_fn1_led_off();
-                        halite_fn0_led_off();
-                        meta_layer_already_on = 0;
-                    } else if (timer_elapsed(key_timer) > META_LAYER_TIMEOUT) {
-                        layer_off(_NUMBERS);
-                        layer_on(_META);
-                        breathing_enable();
-                        halite_fn1_led_on();
-                        halite_fn0_led_on();
-                        meta_layer_already_on = 1;
-                    } else {
-                        number_layer_already_on = 1;
-                    }
-                }
-            }
-        break;
-        case 1:
-            if (record->event.pressed) {
-            } else {
-                breathing_toggle();
-            }
-    }
-    return MACRO_NONE;
-};
 
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_SQUARE_BRACKETS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+    [TD_CURLY_BRACKETS]  = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
+    [TD_PARENTHESES]     = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+    [TD_CHEVRONS]        = ACTION_TAP_DANCE_DOUBLE(KC_LABK, KC_RABK),
+    [TD_QUOTES]          = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
+};
 
 void matrix_init_user(void) {
-}
+    LED_INIT(B5);
+    LED_INIT(F0);
+}    
 
 void matrix_scan_user(void) {
-    uint32_t layer = layer_state;
-
-    if ((1 << _META) == layer) {
+    if (LAYER_ON(_META)) {
+        LED_ON(FN_1_LED);
+        LED_ON(FN_0_LED);
         return;
     }
 
-    if ((1 << _NUMBERS) == layer) {
-        halite_fn1_led_on();
-    } else {
-        halite_fn1_led_off();
-    }
-
-    if ((1 << _SYMBOLS) == layer) {
-        halite_fn0_led_on();
-    } else {
-        halite_fn0_led_off();
-    }
+    BIND_LED_TO_LAYER(FN_1_LED, _NUMBERS);
+    BIND_LED_TO_LAYER(FN_0_LED, _SYMBOLS);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    uint32_t layer = layer_state;
-
-    // Now when releasing the function key, the macro will know
-    // the function key was used a momentary switch and will disable the layer
-    if (((1 << _NUMBERS) == layer) && (keycode != KC_0_5)) {
-        number_layer_already_on = 1;
-    }
-
     return true;
 }
 
